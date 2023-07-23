442746da930144a12cf2d9eac659aee294b16309
ecolazy <ecolazy.pm.me>
2023-07-23 23:07:51 +0100

    test6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,8 +46,8 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/ecolazy',
-          blogSidebar: "all",
+          editUrl:
+            'https://github.com/ecolazy',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
