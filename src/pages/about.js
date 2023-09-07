d81285a3d3b749437e32e5d4bdc9cc0edfbffb96
reuben <reuben@192.168.1.108>
2023-09-07 18:28:49 +0100

    all

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ export default function Hello() {
         <p>
         As a designer with a background in applied technology, I am constantly seeking out new ways to use my skills and knowledge to make a positive impact in the world. My passion for using computation to solve complex problems has led me to become proficient in a variety of areas, including linux, networking, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence.
 
-I believe that by combining the worlds of nature and technology, we can find innovative solutions to the challenges facing society today. For example, using infra-red satellite imagery, we can identify heat loss in buildings and take steps to improve energy efficiency. Data querying can be used to locate, protect, or reinstate wildlife corridors, helping to preserve biodiversity and protect the natural world.
+I believe that by combining the worlds of nature and technology, we can find innovative solutions to the challenges facing society today. For example, using infra-red satellite imagery, we can identify urban heat island effects and take stepts to mitigate this. Data querying can be used to locate, protect, or reinstate wildlife corridors, helping to preserve biodiversity and protect the natural world.
 
 In my professional career, I have gained valuable experience in a range of fields, including architecture and network engineering. I have interned at Heatherwick Studio, a renowned architectural design firm, and have held a position as a network field engineer at Etherlive, a company that provides temporary broadband services for events.
 
